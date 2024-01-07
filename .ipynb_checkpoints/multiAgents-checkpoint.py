32372bc54208f1ff1604f29e7235517592a1fa25
nimiico <nimaeslami206@gmail.com>
2024-01-07 22:15:22 +0330

    Add new Evaluation Functin

--- .ipynb_checkpoints/multiAgents-checkpoint.py
+++ .ipynb_checkpoints/multiAgents-checkpoint.py
@@ -35,94 +35,79 @@ def getLegalActionsNoStop(index, gameState):
             possibleActions.remove(Directions.STOP)
         return possibleActions
 
+def evaluationFunction(currentGameState):
+    score = currentGameState.getScore()
+    pacman = currentGameState.getPacmanPosition()
+    ghost_state = currentGameState.getGhostStates()
+    food = currentGameState.getFood()
+
+    foods = food.asList()
+    food_distances = [manhattanDistance(pacman, food) for food in foods]
+    if food_distances:
+        min_food_distances = min(food_distances)
+        score += 1.0 / min_food_distances
+
+    for ghostState in ghost_state:
+        ghost = ghostState.getPosition()
+        ghost_distance = manhattanDistance(pacman, ghost)
+        if ghost_distance < 1:
+            score -= 100
+
+    return score
+
 
 class MultiAgentSearchAgent(Agent):
     def __init__(self, evalFn="scoreEvaluationFunction", depth="2", time_limit="6"):
-        self.index = 0  # Pacman is always agent index 0
+        self.index = 0
         self.evaluationFunction = util.lookup(evalFn, globals())
         self.depth = int(depth)
         self.time_limit = int(time_limit)
-        self.temperature = 1.0
-
 
 class AIAgent(MultiAgentSearchAgent):
-    def alphabeta(self, agent, depth, gameState, alpha, beta):
+    def alpha_beta(self, agent, depth, gameState, alpha, beta):
         if gameState.isLose() or gameState.isWin() or depth == self.depth:
-            return self.evaluationFunction(gameState), None
+            return evaluationFunction(gameState), None
         possibleActions = getLegalActionsNoStop(agent, gameState)
-        if agent == 0:  # maximize for pacman
-            value = -999999
+        if agent == 0:
+            value = -float("inf")
             action_scores = []
-            for action in getLegalActionsNoStop(agent, gameState):
-                action_score, new_action = self.alphabeta(1, depth, gameState.generateSuccessor(agent, action), alpha,
+            for action in possibleActions:
+                action_score, new_action = self.alpha_beta(1, depth, gameState.generateSuccessor(agent, action), alpha,
                                                           beta)
                 action_scores.append(action_score)
                 value = max(value, action_score)
                 alpha = max(alpha, value)
-                if beta <= alpha:  # alpha-beta pruning
+                if beta <= alpha: 
                     break
             max_action = max(action_scores)
             max_indices = [index for index in range(len(action_scores)) if action_scores[index] == max_action]
             chosenIndex = random.choice(max_indices)
             return value, possibleActions[chosenIndex]
-        else:  # minimize for ghosts
-            nextAgent = agent + 1  # get the next agent
+        else:
+            nextAgent = agent + 1  
             if gameState.getNumAgents() == nextAgent:
                 nextAgent = 0
-            if nextAgent == 0:  # increase depth every time all agents have moved
+            if nextAgent == 0: 
                 depth += 1
             possibleActions = getLegalActionsNoStop(agent, gameState)
-            value = 999999
+            value = float("inf")
             action_scores = []
             for action in possibleActions:
-                action_score, new_action = self.alphabeta(nextAgent, depth, gameState.generateSuccessor(agent, action), alpha, beta)
+                action_score, new_action = self.alpha_beta(nextAgent, depth, gameState.generateSuccessor(agent, action), alpha, beta)
                 action_scores.append(action_score)
+            
                 value = min(value, action_score)
                 beta = min(beta, value)
-                if beta <= alpha:  # alpha-beta pruning
+                if beta <= alpha:
                     break
             min_action = min(action_scores)
             min_indices = [index for index in range(len(action_scores)) if action_scores[index] == min_action]
             chosenIndex = random.choice(min_indices)
-            return value, possibleActions[chosenIndex]
+            return value, None
 
     def getAction(self, gameState: GameState):
-        """
-        Here are some method calls that might be useful when implementing minimax.
-
-        gameState.getLegalActions(agentIndex):
-        Returns a list of legal actions for an agent
-        agentIndex=0 means Pacman, ghosts are >= 1
-
-        gameState.generateSuccessor(agentIndex, action):
-        Returns the successor game state after an agent takes an action
-
-        gameState.getNumAgents():
-        Returns the total number of agents in the game
-
-        gameState.isWin():
-        Returns whether or not the game state is a winning state
-
-        gameState.isLose():
-        Returns whether or not the game state is a losing state
-        """
-
-        # TODO: Your code goes here
-        # util.raiseNotDefined()
-
-        """
-        Returns the expectimax action using self.depth and self.evaluationFunction
-        All ghosts should be modeled as choosing uniformly at random from their
-        legal moves.
-        """
-        """
-        Returns the minimax action from the current gameState using self.depth
-        and self.evaluationFunction using alpha-beta pruning.
-        """
         possibleActions = getLegalActionsNoStop(0, gameState)
-        alpha = -999999
-        beta = 999999
-        #action_scores = [self.alphabeta(0, 0, gameState.generateSuccessor(0, action), alpha, beta) for action
-        #                 in possibleActions]
-        action_score, action = self.alphabeta(0, 0, gameState, alpha, beta)
+        alpha = -float("inf")
+        beta = float("inf")
+        action_score, action = self.alpha_beta(0, 0, gameState, alpha, beta)
         return action
