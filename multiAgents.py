3ead9cb246a2e871790ad1f96dc9b731927f6da8
PouriaTalaei <talaei.pouria06@gmmai.com>
2024-01-08 16:12:15 +0330

    last_edition!

--- multiAgents.py
+++ multiAgents.py
@@ -1,39 +1,22 @@
-# multiAgents.py
-# --------------
-# Licensing Information:  You are free to use or extend these projects for
-# educational purposes provided that (1) you do not distribute or publish
-# solutions, (2) you retain this notice, and (3) you provide clear
-# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-#
-# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
-# The core projects and autograders were primarily created by John DeNero
-# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# Student side autograding was added by Brad Miller, Nick Hay, and
-# Pieter Abbeel (pabbeel@cs.berkeley.edu).
-
-
-from util import manhattanDistance
-from game import Directions
-import random, util
+import random
+import util
 
 from game import Agent
+from game import Directions
 from pacman import GameState
+from util import manhattanDistance
 
 
 def scoreEvaluationFunction(currentGameState: GameState):
-    """
-    This default evaluation function just returns the score of the state.
-    The score is the same one displayed in the Pacman GUI.
-
-    This evaluation function is meant for use with adversarial search agents
-    """
     return currentGameState.getScore()
 
+
 def getLegalActionsNoStop(index, gameState):
-        possibleActions = gameState.getLegalActions(index)
-        if Directions.STOP in possibleActions:
-            possibleActions.remove(Directions.STOP)
-        return possibleActions
+    possibleActions = gameState.getLegalActions(index)
+    if Directions.STOP in possibleActions:
+        possibleActions.remove(Directions.STOP)
+    return possibleActions
+
 
 def evaluationFunction(currentGameState):
     score = currentGameState.getScore()
@@ -63,50 +46,54 @@ class MultiAgentSearchAgent(Agent):
         self.depth = int(depth)
         self.time_limit = int(time_limit)
 
+
 class AIAgent(MultiAgentSearchAgent):
     def alpha_beta(self, agent, depth, gameState, alpha, beta):
         if gameState.isLose() or gameState.isWin() or depth == self.depth:
             return evaluationFunction(gameState), None
-        possibleActions = getLegalActionsNoStop(agent, gameState)
         if agent == 0:
-            value = -float("inf")
-            action_scores = []
-            for action in possibleActions:
-                action_score, new_action = self.alpha_beta(1, depth, gameState.generateSuccessor(agent, action), alpha,
-                                                          beta)
-                action_scores.append(action_score)
-                value = max(value, action_score)
-                alpha = max(alpha, value)
-                if beta <= alpha: 
-                    break
-            max_action = max(action_scores)
-            max_indices = [index for index in range(len(action_scores)) if action_scores[index] == max_action]
-            chosenIndex = random.choice(max_indices)
-            return value, possibleActions[chosenIndex]
+            return self.maxValue(gameState, depth, agent, alpha, beta)
         else:
-            nextAgent = agent + 1  
-            if gameState.getNumAgents() == nextAgent:
-                nextAgent = 0
-            if nextAgent == 0: 
-                depth += 1
-            possibleActions = getLegalActionsNoStop(agent, gameState)
-            value = float("inf")
-            action_scores = []
-            for action in possibleActions:
-                action_score, new_action = self.alpha_beta(nextAgent, depth, gameState.generateSuccessor(agent, action), alpha, beta)
-                action_scores.append(action_score)
-            
-                value = min(value, action_score)
-                beta = min(beta, value)
-                if beta <= alpha:
-                    break
-            min_action = min(action_scores)
-            min_indices = [index for index in range(len(action_scores)) if action_scores[index] == min_action]
-            chosenIndex = random.choice(min_indices)
-            return value, None
+            return self.minValue(gameState, depth, agent, alpha, beta)
+
+    def maxValue(self, gameState, depth, agent, alpha, beta):
+        possibleActions = getLegalActionsNoStop(agent, gameState)
+        value = -float("inf")
+        action_scores = []
+        for action in possibleActions:
+            action_score, new_action = self.alpha_beta(1, depth, gameState.generateSuccessor(agent, action), alpha,
+                                                       beta)
+            action_scores.append(action_score)
+            value = max(value, action_score)
+            alpha = max(alpha, value)
+            if beta < alpha:
+                break
+        max_action = max(action_scores)
+        max_indices = [index for index in range(len(action_scores)) if action_scores[index] == max_action]
+        chosenIndex = random.choice(max_indices)
+        return value, possibleActions[chosenIndex]
+
+    def minValue(self, gameState, depth, agent, alpha, beta):
+        nextAgent = agent + 1
+        if gameState.getNumAgents() == nextAgent:
+            nextAgent = 0
+        if nextAgent == 0:
+            depth += 1
+        possibleActions = getLegalActionsNoStop(agent, gameState)
+        value = float("inf")
+        action_scores = []
+        for action in possibleActions:
+            action_score, new_action = self.alpha_beta(nextAgent, depth, gameState.generateSuccessor(agent, action),
+                                                       alpha, beta)
+            action_scores.append(action_score)
+
+            value = min(value, action_score)
+            beta = min(beta, value)
+            if beta < alpha:
+                break
+        return value, None
 
     def getAction(self, gameState: GameState):
-        possibleActions = getLegalActionsNoStop(0, gameState)
         alpha = -float("inf")
         beta = float("inf")
         action_score, action = self.alpha_beta(0, 0, gameState, alpha, beta)
